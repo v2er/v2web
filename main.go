3427f834b8a7dbde4830a13e82e0397698918b0b
kyai <hukuang@163.com>
2020-01-11 10:26:24 +0800

    beego

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "v2web/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"v2web/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
